5fb9e5fb4ced5f88493bb32a0f390abf6e18a9a9
Desiree <desisant@gmail.com>
2012-12-07 08:07:55 -0300

    Test

--- spec/keyPress_Spec.js
+++ spec/keyPress_Spec.js
@@ -1,6 +1,16 @@
-describe("keyPress", function(){
-
-});
+describe("Testing event keypress with key_enter 13", function(){
 
 
+    var e = jQuery.Event("keydown", { keyCode: 13 });
+
+
+    $('canvas').keydown(function(e)
+    {
+        if (e.keyCode == 13)
+        {
+            alert('Enter was pressed.');
+        }
+    });
+
+}
 
