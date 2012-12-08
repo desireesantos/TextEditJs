739879bd380b3a763834a9ef60e897c21a980fc0
Desiree <desisant@gmail.com>
2012-12-08 10:06:59 -0300

    Class ControlWord

--- spec/keyPress_Spec.js
+++ spec/keyPress_Spec.js
@@ -1,16 +1,20 @@
 describe("Testing event keypress with key_enter 13", function(){
 
-
     var e = jQuery.Event("keydown", { keyCode: 13 });
 
 
-    $('canvas').keydown(function(e)
+    $('canvas').onkeypress(function(e)
     {
         if (e.keyCode == 13)
         {
             alert('Enter was pressed.');
         }
+    } ) ;
+
+    describe("Fizz Buzz", function(){
+        it("numero igual a tres deve retornar fizz", function(){
+            expect(fizzbuzz(3)).toEqual("fizz");
+        });
     });
 
-}
-
+};
---
+++ src/app/control/Main.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: desireesantos
+ * Date: 12/8/12
+ * Time: 1:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function ControlWord (){
+
+   var word ;
+
+}
