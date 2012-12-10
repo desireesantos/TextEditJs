e6d2269f1958f25c9e3cfb7d73b149d073072be7
Desiree <desisant@gmail.com>
2012-12-10 14:52:26 -0200

    update

--- spec/ControlWord.js
+++ spec/ControlWord.js
@@ -12,4 +12,4 @@
 
 };
 
- exports.ControlWord = ControlWord;
+
--- spec/controlWord_Spec.js
+++ spec/controlWord_Spec.js
@@ -2,9 +2,14 @@
 
 describe("ControlWord", function(){
 
-    it("Testing get into word", function() {
+    it("Word is A", function() {
         control = new ControlWord("A");
         expect(control.Word()).toEqual("A");
+    });
+
+    it("word is not null", function() {
+        control = new ControlWord("A");
+        expect(control.Word()).not.toBeNull();
     });
 
 
