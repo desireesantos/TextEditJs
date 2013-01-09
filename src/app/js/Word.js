cb7deac748c5a2a4d282581d6c782e9f7a160257
Desiree <desisant@gmail.com>
2013-01-09 13:09:35 -0200

    Chamada apenas para o evento KeyPress, reponsável por transformar o código unicode  em caracter .

--- src/app/js/Word.js
+++ src/app/js/Word.js
@@ -2,7 +2,7 @@
 Word = function(key) {
 
     this.keyId = key;
-};
+
 
 
     Word.prototype.translateTocharCode = function() {
@@ -10,12 +10,7 @@ Word = function(key) {
         return String.fromCharCode(this.keyID);
     };
 
-
-
-    Word.prototype.translateTocharCode = function() {
-
-    return String.fromCharCode(this.keyID);
-    };
+};
 
 
 
