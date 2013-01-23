c25594bf264272af048829ca8bf81acefd92c579
Desiree <desisant@gmail.com>
2013-01-22 23:19:20 -0200

    Loader dos arquivos Js

---
+++ src/app/js/main.js
@@ -0,0 +1,6 @@
+require(["jquery", "layoutBlackBoard.js", "LinePositionControl.js","EditText.js" ,"Word.js", "Line.js","EspecialKeys.js"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+        $('body').alpha().beta();
+    });
+});
