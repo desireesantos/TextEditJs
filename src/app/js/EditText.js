c3d58cd78bcd54de184d59bfe2340640cde49dd3
Desiree <desisant@gmail.com>
2013-01-23 23:24:16 -0200

    Imprime as letras no canvas

--- src/app/js/EditText.js
+++ src/app/js/EditText.js
@@ -1,32 +1,20 @@
     document.onkeypress = keyPress;
-
     var context = document.getElementById("canvas").getContext("2d");
     var keyId = event.keyCode;
 
+       function keyPress()
+        {
+            var word = new Word(keyId);
+                word.insertWord();
+        }
 
-
-    function keyPress()
-
-    {
-
-        var word = new Word(keyId);
-        word.insertWord();
-
-
-    }
 
 
 
     document.onkeydown = keyDown;
+        function keyDown()
 
-
-
-    function keyDown()
-
-    {
-
-        var word = new EspecialKeys(keyId);
-        word.insertWord();
-
-
-    }
+        {
+            var word = new EspecialKeys(keyId);
+                word.insertWord();
+        }
