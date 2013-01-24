a1650688b543d094423786b5eb0f4dcfba59c2db
Desiree <desisant@gmail.com>
2013-01-23 23:25:03 -0200

    Imprime as letras no canvas

--- src/app/js/EspecialKeys.js
+++ src/app/js/EspecialKeys.js
@@ -1,31 +1,22 @@
+    EspecialKeys = function(key) {
 
-EspecialKeys = function(key) {
-
-    var backspace = 8;
-    var tab       = 9;
-    var enter     = 13;
-
-    var line = new Line();
-    
-
-    switch(key)
-    {
-        case backspace:
-              line.backspacePosition();break;
-
-        case tab:
-            line.tabPosition();break;
-
-        case enter:
-            line.nextLine();break;
+        var backspace = 8;
+        var tab       = 9;
+        var enter     = 13;
+        var line = new Line();
 
 
-        default:
-            return false;
-    }
+        switch(key)
+         {
+            case backspace:
+                    line.backspacePosition();break;
 
+            case tab:
+                    line.tabPosition();break;
 
-
+            case enter:
+                    line.nextLine();break;
+         }
 };
 
 
