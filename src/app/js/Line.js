b3827a9d8036d8f9add2c9c30de95559f1b716c1
Desiree <desisant@gmail.com>
2013-01-23 23:31:34 -0200

    Incluindo funcionalidade para o evento keyDown (teclas: enter, tab e backspace)

--- src/app/js/Line.js
+++ src/app/js/Line.js
@@ -1,46 +1,42 @@
-
 var Line = function(word)
 {
     var line =[];
-    var coordinateXPosition;
-
-
+    var coordinateXPosition
     var linePositionControl = new LinePositionControl(word);
 
-    this.insertWord = function(){
 
-        line.push(word);
+    this.insertWord = function()
+        {
+            line.push(word);
+            linePositionControl.currentPositionX();
+           return line;
+        }
 
-        linePositionControl.currentPositionX();
-        return line;
 
-    }
+     this.removeWord = function()
+        {
 
+            line.pop();
+           return line;
+        }
 
-    this.removeWord = function(){
-
-        line.pop();
-        return line;
-
-    }
 
     this.backspacePosition = function ()
-    {
-        linePositionControl.backspacePosition();
+        {
+            linePositionControl.backspacePosition();
+        }
 
-    }
 
     this.nextLine = function ()
-    {
-        linePositionControl.nextLine();
+        {
+            linePositionControl.nextLine();
+        }
 
-    }
 
     this.tabPosition = function ()
-    {
-        linePositionControl.tabPosition();
-
-    }
+        {
+            linePositionControl.tabPosition();
+        }
 };
 
 
