5d1885f7af5fac8d171463ab9bbdef4cc386ae85
Desiree <desisant@gmail.com>
2013-01-23 23:33:01 -0200

    Atualiza a coordenada (x,y) na Linha que  e imprime a letra

--- src/app/js/LinePositionControl.js
+++ src/app/js/LinePositionControl.js
@@ -1,48 +1,32 @@
-var LinePositionControl = function (word){
+var LinePositionControl = function (word) {
 
 
-    var marginLeft= 180;
-    var marginRight= 800;
-    var textPosition = 5;
-    var textDirection ="right";
+    var coordinateY = 180;
+    var marginRight = 980;
+    var coordinateXPosition;
 
 
-
-    this.currentPositionInLine = function(){
-
+    this.currentPositionX = function ()
+        {
 
 
             var textWidth = context.measureText(word).width;
 
+                coordinateXPosition < (coordinateXPosition - textWidth) ? coordinateXPosition + 10 : coordinateXPosition = 0;
+                printWord();
 
-            if (textDirection == "right") {
-                textPosition += 10;
-
-                if (textPosition > marginRight - textWidth) {
-                    textDirection = "left";
-                }
-
-            }
-            else {
-                textXpos = 0;
-
-                if (textPosition < 10) {
-                    textDirection = "right";
-                }
-
-            }
-
-        printWord();
-        return textPosition;
+            return coordinateXPosition;
 
         };
 
 
 
-    var printWord = function(){
 
-        context.fillText(word, textPosition, marginLeft);
-    }
+    var printWord = function()
+        {
+
+            context.fillText(word , coordinateXPosition, coordinateY);
+        }
 
 
 };
